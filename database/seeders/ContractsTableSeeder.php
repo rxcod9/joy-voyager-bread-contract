40b25432351b41e0610e01ea3544fd788719afc3
Ramakant <gangwar.ramakant@gmail.com>
2022-05-03 19:24:06 +0530

    💚_Seeder-count

--- database/seeders/ContractsTableSeeder.php
+++ database/seeders/ContractsTableSeeder.php
@@ -18,7 +18,7 @@ class ContractsTableSeeder extends Seeder
             return false;
         }
 
-        $count = 100;
+        $count = 20;
         Contract::factory()
             ->count($count)
             ->state(function (array $attributes) use ($count) {
--- database/seeders/DummyContractsTableSeeder.php
+++ database/seeders/DummyContractsTableSeeder.php
@@ -14,7 +14,7 @@ class DummyContractsTableSeeder extends Seeder
      */
     public function run()
     {
-        $count = 100;
+        $count = 20;
         Contract::factory()
             ->count($count)
             ->state(function (array $attributes) use ($count) {
